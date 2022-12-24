445cdc8d5044314d1d2ee97db8847629a40ce069
David Schnurr <dschnurr@openai.com>
2022-12-24 11:50:51 -0600

    error on empty input

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -23,10 +23,25 @@ export default async function (req, res) {
     return;
   }
 
+  const animal = req.body.animal || '';
+  if (animal.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid animal",
+      }
+    });
+    return;
+  }
+
   try {
     const completion = await openai.createCompletion({
+<<<<<<< HEAD
       model: "text-davinci-002",
       prompt: generatePrompt(req.body.animal),
+=======
+      model: "text-davinci-003",
+      prompt: generatePrompt(animal),
+>>>>>>> 259a13a (error on empty input)
       temperature: 0.6,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
