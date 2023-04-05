c8f41c605aae5fc1af8f914d82d8b594eb756265
ckwwingo <99191737+wingck@users.noreply.github.com>
2023-04-05 11:55:45 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function colorBody() {
-    document.body.style.background  = document.getElementsByName("bodycolor")[0].value;
-}
-function colorHeader(){
-    var myHeader = document.getElementById("header-menu")
-    myHeader.style.background  = document.getElementsByName("headercolor")[0].value;
-}
